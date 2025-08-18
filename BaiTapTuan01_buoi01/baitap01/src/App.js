55e0c90e7681dda82e57b6036d0d39381269736f
nguyenthianhhuyen03@gmail.com <nguyenthianhhuyen03@gmail.com>
2025-08-18 11:11:41 +0700

    bai tap tuan 1

---
+++ BaiTapTuan01_buoi01/baitap01/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{
+      fontFamily: "Arial, sans-serif",
+      textAlign: "center",
+      marginTop: "50px"
+    }}>
+      <h1>Thông tin cá nhân</h1>
+      <p><strong>Họ và tên:</strong> Nguyễn Thị Ánh Huyền</p>
+      <p><strong>MSSV:</strong> 21110476</p>
+      <p><strong>Khoa:</strong> Công nghệ thông tin</p>
+      <p><strong>Email:</strong> nguyenthianhhuyen03@gmail.com</p>
+    </div>
+  );
+}
+
+export default App;
